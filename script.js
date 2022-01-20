816b18a4356a5fd8f6ddc1c014a4b10c8f7ea2ee
dreinasant <ao.andreina@gmail.com>
2022-01-20 12:27:54 -0300

    Desafio-de-logica-02

---
+++ script.js
@@ -0,0 +1,21 @@
+//let idade = 21
+//if(idade > 18){
+   // console.log("Você é maior de idade, ja pode sair sozinho(a)!!")
+//}
+
+let idade = 20
+if(humana = true){
+    console.log("A saida é true!")
+}
+
+let mes = "setembro"
+if(mes == "janeiro" || mes == "setembro"){
+    console.log("Que legal, você faz aniversario em setembro")
+}
+
+let nome = "Andreina"
+if(nome.substring (0,1) == "R"){
+    console.log("Seu nome começa com a letra R")
+}else{
+    console.log("Seu nome não começa com a letra R")
+}
