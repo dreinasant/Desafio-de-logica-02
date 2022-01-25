9ada865b6c53a8dbd1452b83609e3412d038af3c
dreinasant <ao.andreina@gmail.com>
2022-01-25 15:13:59 -0300

    atualização

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-//let idade = 21
-//if(idade > 18){
-   // console.log("Você é maior de idade, ja pode sair sozinho(a)!!")
-//}
-
-let idade = 20
-if(humana = true){
-    console.log("A saida é true!")
+let idade = 21
+if(idade > 18){
+   console.log("Você é maior de idade, ja pode sair sozinho(a)!!")
 }
+
+let humana = true
+if(idade > 18 && humana){
+    console.log("Não sou um robo!")
+}
+
 
 let mes = "setembro"
 if(mes == "janeiro" || mes == "setembro"){
@@ -18,4 +19,13 @@ if(nome.substring (0,1) == "R"){
     console.log("Seu nome começa com a letra R")
 }else{
     console.log("Seu nome não começa com a letra R")
+}
+
+
+let sobreNome = "Santos"
+nome = "Andreina"
+if(sobreNome.length > 6 || nome.substring (0,1) == "E"){
+    console.log(`${nome} seu sobrenome tem mais de 6 letras ou seu nome começa com a letra E`)
+}else{
+    console.log(`${nome} seu sobrenome não tem mais de 6 letras ou seu nome não começa com a letra E`)
 }
